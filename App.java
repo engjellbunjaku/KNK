6a7cbda433201ec6338b64c5c29217b847f753ef
Joni Krasniqi <krasniqijoni2002@gmail.com>
2022-05-05 22:56:03 +0200

    initial commit

---
+++ App.java
@@ -0,0 +1,22 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent parent = FXMLLoader.load(getClass().getResource("views/login.fxml"));
+    Scene scene = new Scene(parent);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+}
